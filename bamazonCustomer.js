927bda830200f051cc3c9af341d193f72090d7d5
Wills Grave <will@keydigitalconsulting.com>
2019-10-25 23:49:52 -0400

    Added the code from activity 7 in order to pull the table into the terminal.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,13 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  inventory();
+  afterConnection();
 });
 
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
